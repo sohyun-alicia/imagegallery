0fc707fd9d99abd1a3d2398f46d31852bae800be
sohyun-alicia <78770235+sohyun-alicia@users.noreply.github.com>
2021-03-30 17:46:27 +0900

    gallery project

--- script.js
+++ script.js
@@ -1,9 +1,64 @@
-var req = new XMLHttpRequest();             // 객체 생성
-req.open("GET", "./json/image_list.json");  // 이미지 파일을 얻어오도록 설정
-req.onreadystatechange = function(){        // 콜백 함수 등록
-    if (this.readyState == 4){              // 모든 데이터가 정상으로 수신되었을 때
-        console.log(this.response);         // 응답 출력
+var req = new XMLHttpRequest();
+req.open("GET", "./json/image_list.json");
+req.onreadystatechange = function() {
+  if (this.readyState == 4) {
+    // console.log(this.response);
+    var data = JSON.parse(this.response);
+    for (var i = 0; i < data.length; i++) {
+      var div = document.createElement("div");
+      div.setAttribute("class", "image");
+      div.onclick = function () {
+        this.classList.toggle("image-selected");
+      }
+      div.onmouseover = function () {
+        var element = this;
+        this.timerId = setTimeout(function() {
+          element.classList.add("image-magnified");
+        }, 1000);
+      }
+      div.onmouseout = function() {
+        clearTimeout(this.timerId);
+        this.classList.remove("image-magnified");
+      }
+      var img = document.createElement("img");
+      img.src = data[i];
+      div.appendChild(img);
+      document.body.appendChild(div);
     }
+  }
 }
-req.send()                                  // 해당 요청을 전송합니다.
+req.send();
 
+function selectAll(btn) {
+  var images = document.getElementsByClassName("image");
+  for (var i = 0; i < images.length; i++ ) {
+    if (btn.value == "Unselect All") {
+      images[i].classList.remove("image-selected");
+    } else {
+      images[i].classList.add("image-selected");  
+    }
+  }
+  if (btn.value == "Unselect All") {
+    btn.value = "Select All";
+  } else {
+    btn.value = "Unselect All";
+  }
+}
+
+function slideShow(btn) {
+    var images = document.getElementsByClassName("image");
+    var index = 0;
+    images[index].classList.add("image-magnified");
+  
+    var intervalId = setInterval(function() {
+      images[index].classList.remove("image-magnified");
+      index++;
+      if (index < images.length) {
+        images[index].classList.add("image-magnified");
+      } 
+      else {
+        clearInterval(intervalId);
+      }
+    }, 1000);
+
+}
